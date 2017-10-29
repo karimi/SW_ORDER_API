8258baea3eddbc196dbbda1be5cd311ae0534db7
Morteza Karimi <mortza@gmail.com>
2017-10-29 18:48:18 -0400

    shell ready

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, session
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def main():
+    return render_template("main.html")
 
 if __name__ == "__main__":
     app.run()
